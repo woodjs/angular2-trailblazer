773319079a076f2ed74ac1da9164b721cb156222
yinyanlv <yanlv.yin@gmail.com>
2016-10-13 10:08:25 +0800

    提交修改

--- app/display_data/app.component.ts
+++ app/display_data/app.component.ts
@@ -5,26 +5,15 @@ import {Hero} from './hero';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <h2>我最喜欢的英雄是: {{myHero.name}}</h2>
-    <p>英雄列表：</p>
-    <ul>
-      <li *ngFor="let hero of heros">
-        id: {{hero.id}} | name: {{hero.name}}
-      </li>    
-    </ul>
-    <p *ngIf="heros.length > 4">英雄人数大于3<p>
-</p>
+   <input type="text" (keyup)="1111" #a > 
+   {{a.value}}
   `
 })
 
 export class AppComponent {
-  title = '这里是标题';
-  heros = [
-    new Hero(1, 'a'),
-    new Hero(2, 'b'),
-    new Hero(3, 'c'),
-    new Hero(4, 'd')
-  ];
-  myHero = this.heros[0];
+  inputMessage = '';
+
+  onInput(e) {
+    this.inputMessage += e.target.value + '|';
+  }
 }
